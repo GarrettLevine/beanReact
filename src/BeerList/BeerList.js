ab7aad5b1b5e7c009f33a693fa2a8e97ed8a67bc
Garrett Levine <garrlevine@gmail.com>
2017-04-02 17:45:32 -0400

    returning data in the state

--- src/BeerList/BeerList.js
+++ src/BeerList/BeerList.js
@@ -4,11 +4,26 @@ class BeerList extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      beerList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.punkapi.com/v2/beers', {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        beerList: data,
+      });
+    });
   }
 
   render() {
     return (
-      <div class="columns">
+      <div className="columns">
 
       </div>
     );
