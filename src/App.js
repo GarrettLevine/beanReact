0931a96d649e7c80facbec56018f674d637d46a5
Garrett Levine <garrlevine@gmail.com>
2017-04-02 16:06:56 -0400

    beer list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import BeerList from './BeerList/BeerList';
+
 import './App.css';
 
 class App extends Component {
@@ -7,6 +10,7 @@ class App extends Component {
       <div className="column">
         <h1 className="title is-1">Beans Love Beers</h1>
         <p className="subtitle is-4">A small app to keep track of your beany beers.</p>
+        <BeerList />
       </div>
     );
   }
---
+++ src/BeerList/BeerList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BeerList extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div class="columns">
+
+      </div>
+    );
+  }
+}
+
+export default BeerList;
