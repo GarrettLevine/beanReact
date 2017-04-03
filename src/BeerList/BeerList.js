7c0d537a7fcb4535965d2fa9781d2ae7b647fdfa
Garrett Levine <garrlevine@gmail.com>
2017-04-02 20:48:22 -0400

    beerList with search bar

--- src/BeerList/BeerList.js
+++ src/BeerList/BeerList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
+import './BeerList.css';
+
 class BeerList extends Component {
   constructor(props) {
     super(props);
+    this.renderBeers = this.renderBeers.bind(this);
 
     this.state = {
       beerList: [],
@@ -10,7 +13,7 @@ class BeerList extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.punkapi.com/v2/beers', {
+    fetch('https://api.punkapi.com/v2/beers?per_page=24', {
       method: 'GET',
     })
     .then(res => res.json())
@@ -21,10 +24,66 @@ class BeerList extends Component {
     });
   }
 
+  renderBeers() {
+    return this.state.beerList.map(beer => {
+      const formatDescription = string => {
+        if (string < 100) return string;
+
+        return `${string.slice(0, 100)}...`;
+      };
+
+      return (
+        <div key={beer.id} className="beerCard box">
+          <article className="media">
+            <div className="media-left">
+              <figure className="image is-96x96">
+                <img src={beer.image_url} alt={beer.name} />
+              </figure>
+            </div>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>{beer.name}</strong>
+                  <br />
+                  {formatDescription(beer.description)}
+                </p>
+              </div>
+              <nav className="level is-mobile">
+                <div className="level-left">
+                  <a className="level-item button is-primary">
+                    <span className="icon is-small">
+                      <i className="fa fa-star-o" />
+                    </span>
+                    <span className="is-small">Favourite</span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </article>
+        </div>
+        
+      );
+    });
+  }
+
   render() {
     return (
-      <div className="columns">
-
+      <div>
+        <div className="field has-addons has-addons-centered">
+          <p className="beerSearch control">
+            <input
+              className="input is-medium"
+              type="text"
+              placeholder="Search for beer..."
+            />
+          </p>
+          <p className="control">
+            <a className="button is-info is-medium">Search</a>
+          </p>
+        </div>
+        <div className="beerList">
+          {this.renderBeers()}
+        </div>
       </div>
     );
   }
