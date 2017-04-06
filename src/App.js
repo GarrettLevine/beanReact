a7281e4c3052165d140b1761054eac4566c37edf
Garrett Levine <garrlevine@gmail.com>
2017-04-05 22:44:28 -0400

    starter state

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="column">
-        <h1 className="title is-1">Beans Love Beers</h1>
-        <p className="subtitle is-4">A small app to keep track of your beany beers.</p>
-        <BeerList />
+       
       </div>
     );
   }
