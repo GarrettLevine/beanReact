08e3bb5a49f2816a11acafe8a5605ff72e008c40
Garrett Levine <garrlevine@gmail.com>
2017-04-08 16:39:03 -0400

    borked it

--- src/FavouriteList/FavouriteList.js
+++ src/FavouriteList/FavouriteList.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class FavouriteList extends Component {
   render() {
     return (
       <section className="section">
         <h1 className="title is-4">Favourite Beerz</h1>
+        <p>{this.props.searchValue}</p>
       </section>
     );
   }
 }
 
-export default FavouriteList;
+function mapStateToProps(state) {
+ return {
+   searchValue: state.beer.searchInput,
+ }
+};
+
+export default connect(
+  mapStateToProps,
+)(FavouriteList);
